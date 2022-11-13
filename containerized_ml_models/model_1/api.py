86fcc99f8897e211b26b1551c9f2605af06f1836
MattS0000 <mati201420@gmail.com>
2022-11-13 13:15:43 +0100

    Black/Flake correction

--- containerized_ml_models/model_1/api.py
+++ containerized_ml_models/model_1/api.py
@@ -6,21 +6,23 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/ping')
+@app.route("/ping")
 def ping_success():
-    return jsonify({'ping': 'success'})
+    return jsonify({"ping": "success"})
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     record = request.get_data(as_text=True)
     data = np.array(json.loads(record))
     if len(data.shape) != 2 or data.shape[1] != 10:
-        raise ValueError("Illegal Argument of incorrect shape. Input should be a list of shape (n,10).")
-    with open('model.pkl', 'rb') as model_bin:
+        raise ValueError(
+            "Illegal Argument of incorrect shape. Input should be a list of shape (n,10)."
+        )
+    with open("model.pkl", "rb") as model_bin:
         model = pickle.load(model_bin)
         prediction = model.predict(data)
-    return jsonify({'Result': f'{prediction}'})
+    return jsonify({"Result": f"{prediction}"})
 
 
 if __name__ == "__main__":
