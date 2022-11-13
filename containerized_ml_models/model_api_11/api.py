6e8b439ff6418b14790535c295207d8250a24df1
Igor Kraszewski <91636931+kraszor@users.noreply.github.com>
2022-11-13 18:50:24 +0100

    Update api.py

--- containerized_ml_models/model_api_11/api.py
+++ containerized_ml_models/model_api_11/api.py
@@ -5,24 +5,24 @@ import ast
 app = Flask(__name__)
 
 
-@app.route('/ping', methods=['GET'])
+@app.route("/ping", methods=["GET"])
 def ping():
     return jsonify({"ping": "success"})
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
     prediction = None
-    if request.method == 'POST':
-        data = request.get_data().decode('ascii')
+    if request.method == "POST":
+        data = request.get_data().decode("ascii")
         data = ast.literal_eval(data)
-        model = pickle.load(open('model.pkl', 'rb'))
+        model = pickle.load(open("model.pkl", "rb"))
         try:
             prediction = model.predict(data).tolist()
         except Exception:
             prediction = None
-    return jsonify({'Result': prediction})
+    return jsonify({"Result": prediction})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
