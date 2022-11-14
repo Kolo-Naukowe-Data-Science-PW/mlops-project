18b1ea8c39827d165ec24da1cba2f8f4d4c2b686
DawidPludowski <dawid.pludowski@gmail.com>
2022-11-15 00:10:01 +0100

    add predict-api (6)

---
+++ containerized_ml_models/docker_api_6/api_predict.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from constants import MODEL_PATH
+import pickle as pkl
+import numpy as np
+import json
+
+app = Flask(__name__)
+
+with open(MODEL_PATH, "rb") as f:
+    model = pkl.load(f)
+
+
+@app.route("/ping", methods=["GET"])
+def get():
+    return jsonify({"ping": "success"})
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    content = request.values
+    input_data = json.loads(list(content)[0])
+    prediction = model.predict(input_data).tolist()
+    return jsonify({"Result": prediction})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
