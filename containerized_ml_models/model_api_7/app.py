82ebeaa9cfeeb342a18015d7397dd3c847b3f3eb
adamdabkowski <adamdobrus@gmail.com>
2022-11-15 10:38:24 +0100

    app.py formatted

--- containerized_ml_models/model_api_7/app.py
+++ containerized_ml_models/model_api_7/app.py
@@ -7,18 +7,18 @@ import pickle
 app = Flask(__name__)
 
 
-@app.route('/ping', methods=['GET'])
+@app.route("/ping", methods=["GET"])
 def ping():
     return jsonify({"ping": "success"})
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = json.loads(request.get_data())
-    model = pickle.load(open('model.pkl', 'rb'))
+    model = pickle.load(open("model.pkl", "rb"))
     prediction = model.predict(data).tolist()
-    return jsonify({'Result': prediction})
+    return jsonify({"Result": prediction})
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host="127.0.0.1", port=5000)
