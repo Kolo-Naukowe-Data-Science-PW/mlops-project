454f3a94b680999ce8528f7ae5738efb4fab0eda
Sebastian Rydz <TheRealSeber>
2022-11-17 22:07:11 +0100

    a

--- containerized_ml_models/model_api_10/api.py
+++ containerized_ml_models/model_api_10/api.py
@@ -1,24 +1,24 @@
 import json
 import pickle
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-with open("model.pkl", "rb") as file:
+with open("model/model.pkl", "rb") as file:
     pickle_in = pickle.load(file)
 
 
 @app.route("/ping", methods=["GET"])
 def ping():
-    return json.dumps({"ping": "success"})
+    return jsonify({"ping": "success"})
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     record = json.loads(request.get_data())
     pr = pickle_in.predict(record).tolist()
-    return json.dumps({"Result": pr})
+    return jsonify({"Result": pr})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
