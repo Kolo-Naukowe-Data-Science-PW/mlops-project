878bce71eb1c21be25c9e8f57c3e8e7b3aff3c65
HermanPlay <hermanplay1110@gmail.com>
2022-11-17 20:06:43 +0100

    added if main==name

--- containerized_ml_models/model_api_4/api.py
+++ containerized_ml_models/model_api_4/api.py
@@ -23,4 +23,9 @@ def read_model(data):
     return model.predict(data).tolist()
 
 
-app.run(port=8000, host="0.0.0.0")
+def main():
+    app.run(port=8000, host="0.0.0.0")
+
+
+if __name__ == "__main__":
+    main()
