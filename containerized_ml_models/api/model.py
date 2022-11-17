d6b18198b772267412239d27b446d9b193e609dd
Antoni Zajko <44975359+azoz01@users.noreply.github.com>
2022-11-17 21:16:13 +0100

    Update model.py

--- containerized_ml_models/api/model.py
+++ containerized_ml_models/api/model.py
@@ -5,11 +5,11 @@ import numpy as np
 
 class Model:
 
-    __create_key = object()
+    __creation_key = object()
     class_instance = None
 
-    def __init__(self, model_path: str, create_key: object) -> None:
-        if create_key is not self.__create_key:
+    def __init__(self, model_path: str, creation_key: object) -> None:
+        if creation_key is not self.__creation_key:
             raise ValueError(
                 "You shouldn't call __init__ method. "
                 "Call Model.get_or_create instead"
@@ -24,5 +24,5 @@ class Model:
     def get_or_create(cls, model_path: str) -> Model:
         if cls.class_instance:
             return cls.class_instance
-        cls.class_instance = cls(model_path, cls.__create_key)
+        cls.class_instance = cls(model_path, cls.__creation_key)
         return cls.class_instance
