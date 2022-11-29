98c500d2e1f4327a92a793cdda75678bfe2a5fcd
KubaKrzych <krzychowiec.k@gmail.com>
2022-11-29 08:51:11 +0100

    Modify ./k8s and bootstrap.sh

--- tasks/02_kubernetes_fundamentals/kubernetes_1/bootstrap.sh
+++ tasks/02_kubernetes_fundamentals/kubernetes_1/bootstrap.sh
@@ -3,6 +3,11 @@ if [ -x "$(command -v docker)" ]; then
         dir=$(basename "$dir")
         docker build -t mlops-$dir":"latest --build-arg model="$dir" "$PWD/models/." 
     done
+    minikube start -p ingress-cluster
+    eval $(minikube -p minikube docker-env)
+    address=$(minikube ip -p ingress-cluster)
+    echo "Aby połączyć się z serwerem wejdź pod adres: $address"
 else
     echo "Install docker!"
 fi
+
