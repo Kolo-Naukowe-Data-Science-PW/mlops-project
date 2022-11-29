99c35a18edbdc3307da875cf50c7074c5cc2f09f
HermanPlay <hermanplay1110@gmail.com>
2022-11-29 13:27:39 +0100

    fixed problems with ingress, changed a bit build.py script, to build only our models

--- tasks/02_kubernetes_fundamentals/kubernetes_2/build.py
+++ tasks/02_kubernetes_fundamentals/kubernetes_2/build.py
@@ -3,7 +3,8 @@ import os
 MY_DIR = os.path.abspath(os.curdir)
 IMAGES_DIR = os.path.join(MY_DIR, "..", "..", "01_containerized_ml_models")
 
-if __name__ == "__main__":
+
+def main():
     # start cluster (uncomment if not set via docker desktop)
     # os.system("minikube start")
     # os.system("minikube docker-env")
@@ -12,14 +13,20 @@ if __name__ == "__main__":
     # build docker images
     for folder in os.listdir(IMAGES_DIR):
         if folder.startswith("model_api_"):
-            name = os.path.splitext(folder)[0].split("_")[-1]
-
-            os.system(
-                f"docker build -t model-{name} {os.path.join(IMAGES_DIR, folder)}"
-            )
+            number = os.path.splitext(folder)[0].split("_")[-1]
+            # build only images which are needed
+            if number in ["4", "16"]:
+                os.system(
+                    f"docker build -t model-{number} \
+                    {os.path.join(IMAGES_DIR, folder)}"
+                )
 
     # deploy pods
     for file in os.listdir(MY_DIR):
         if file.startswith("model"):
             os.system(f"kubectl apply -f {file}")
-    os.system(f"kubectl apply -f ingress.yaml")
+    os.system("kubectl apply -f ingress.yaml")
+
+
+if __name__ == "__main__":
+    main()
