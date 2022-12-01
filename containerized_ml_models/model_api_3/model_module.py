d74fbc85a92096b4c186cf9b101e4d4f9bfee53b
TheRealSeber <111927572+TheRealSeber@users.noreply.github.com>
2022-12-01 01:29:30 +0100

    Delete containerized_ml_models directory
    Deleted unused ml_models

--- containerized_ml_models/model_api_3/model_module.py
+++
@@ -1,9 +0,0 @@
-"""
-Using the fact that module is imported only once,
-we get pseudo-singleton behavior by importing model_module
-"""
-import pickle
-
-
-with open("model.pkl", "rb") as file:
-    model = pickle.load(file)
