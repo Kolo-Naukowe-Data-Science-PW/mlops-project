979bc194fa519df2594fcf70a005bb8e32be73c4
radekaadek <radekaadek@gmail.com>
2022-12-03 13:38:41 +0100

    odpala sie python

--- tasks/02_kubernetes_fundamentals/kubernetes_2/build.py
+++ tasks/02_kubernetes_fundamentals/kubernetes_2/build.py
@@ -10,22 +10,19 @@ def main():
     # os.system("minikube docker-env")
     # os.system("eval $(minikube -p minikube docker-env)")
 
+    models = {'4', '8', '16'}
+    
     # build docker images
-    for folder in os.listdir(IMAGES_DIR):
-        if folder.startswith("model_api_"):
-            number = os.path.splitext(folder)[0].split("_")[-1]
-            # build only images which are needed
-            if number in ["4", "16", "8"]:
-                os.system(
-                    f"docker build -t model-{number} \
-                    {os.path.join(IMAGES_DIR, folder)}"
-                )
+    for model in models:
+        os.chdir(os.path.join(IMAGES_DIR, f"model_api_{model}"))
+        os.system(f"docker build -t model-{model} .")
 
     # deploy pods
-    for file in os.listdir(MY_DIR):
-        if file.startswith("model"):
-            os.system(f"kubectl apply -f {file}")
+    os.chdir(MY_DIR)
+    for model in models:
+        os.system(f"kubectl apply -f model-{model}.yaml")
     os.system("kubectl apply -f ingress.yaml")
+    
 
 
 if __name__ == "__main__":
